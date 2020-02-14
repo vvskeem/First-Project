20f3d78da9b05f63f80fad2173e583349f3ee57b
Shakeem <albouyshakeem@gmail.com>
2020-02-14 11:50:13 -0400

    new updates

--- index.js
+++ index.js
@@ -1,51 +1,43 @@
-$('#newMusic').on('click', function (event) {
+$("#newMusic").on("click", function(event) {
+  let spotifySearch = $(event.target).attr("name");
+  console.log(spotifySearch);
 
-    let spotifySearch = $(event.target).attr('name')
-    console.log(spotifySearch)
+  let spotifyApi = "https://spotifykeem.herokuapp.com/" + spotifySearch;
+  console.log(spotifyApi);
 
-    let spotifyApi = 'https://spotifykeem.herokuapp.com/' + spotifySearch
-    console.log(spotifyApi)
+  $.get(spotifyApi, function(response) {
+    $("#music").empty();
+    for (let index = 0; index < response.length; index++) {
+      const element = response[index];
+      let { name, popularity, preview_url, image } = element;
 
-    $.get(spotifyApi, function (response) {
+      let card = $("<div>").attr({
+        class: "musicCard"
+      });
 
-        $("#music").empty()
-        for (let index = 0; index < response.length; index++) {
-            const element = response[index];
-            let {
-                name,
-                popularity,
-                preview_url,
-                image
-            } = element
+      let cardPoster = $("<img>").attr({
+        src: image,
+        class: "card-img-top"
+      });
+      let audioSrc = $("<audio controls>").attr({
+        src: preview_url,
+        type: "audio/mp3"
+      });
 
-            let card = $("<div>").attr({
-                class: "musicCard"
-            })
+      let cardBody = $("<div>").attr("class", "card-body");
+      let cardYear = $("<p>")
+        .attr("class", "card-text")
+        .html(popularity);
+      let cardTitle = $("<h5>").html(name);
 
-            let cardPoster = $("<img>").attr({
-                src: image,
-                class: "card-img-top",
+      cardBody
+        .append(cardTitle)
+        .append(cardYear)
+        .append(audioSrc);
 
-            })
-            let audioSrc = $("<audio controls>").attr({
-                src: preview_url,
-                type: "audio/mp3"
-            })
+      card.append(cardPoster).append(cardBody);
 
-
-            let cardBody = $("<div>").attr("class", "card-body")
-            let cardYear = $("<p>").attr("class", "card-text").html(popularity)
-            let cardTitle = $("<h5>").html(name)
-
-            cardBody.append(cardTitle).append(cardYear).append(audioSrc);
-
-            card.append(cardPoster).append(cardBody)
-
-
-            $("#music").append(card)
-        }
-
-    })
-
-
-})
+      $("#music").append(card);
+    }
+  });
+});
